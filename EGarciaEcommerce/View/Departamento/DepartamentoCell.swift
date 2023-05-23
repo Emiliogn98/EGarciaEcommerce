fa612693105f58d0b32e012c5e4fa5a5350e096a
Emiliogn98 <Emiliogn.98@gmail.com>
2023-05-23 13:54:37 -0600

    vistas  ventas, antes de modificar nombre a celda

---
+++ EGarciaEcommerce/View/Departamento/DepartamentoCell.swift
@@ -0,0 +1,23 @@
+//
+//  DepartamentoCell.swift
+//  EGarciaEcommerce
+//
+//  Created by MacBookMBA4 on 23/05/23.
+//
+
+import UIKit
+
+class DepartamentoCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var imagenView: UIImageView!
+    
+    @IBOutlet weak var lblNombre: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
